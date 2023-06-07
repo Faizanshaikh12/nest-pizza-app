acb678b7c99cbe2c74a6b705b195cb97d406f7e0
Faizan Shaikh <107682725+Faizanshaikh35@users.noreply.github.com>
2023-06-07 23:02:56 +0530

    - Mongo db Connection - User Module - User Schema

---
+++ src/app/users/users.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '../../schemas/user.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {
+  }
+}
