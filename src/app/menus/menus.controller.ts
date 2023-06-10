76845c5f09128e04655973784e8da874ae0c9b25
Bhanderi Vaibhav <vaibhavbhanderi5@gmail.com>
2023-06-10 11:18:28 +0530

    Menu listing api

--- src/app/menus/menus.controller.ts
+++ src/app/menus/menus.controller.ts
@@ -1,9 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from "@nestjs/common";
 import { MenusService } from './menus.service';
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { Menu } from "../../schemas/menu.schema";
 
 @ApiTags('menus')
 @Controller('menus')
 export class MenusController {
   constructor(private readonly menusService: MenusService) {}
+
+  @Get('')
+  @ApiOperation({ summary: 'List Menu' })
+  async getMenus(): Promise<Menu[]> {
+    return this.menusService.getMenus();
+  }
+
 }
--- src/app/menus/menus.service.ts
+++ src/app/menus/menus.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { Menu } from "../../schemas/menu.schema";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
 
 @Injectable()
-export class MenusService {}
+export class MenusService {
+  constructor(@InjectModel(Menu.name) private menuModel: Model<Menu> ) {
+  }
+
+  async getMenus(): Promise<Menu[]>{
+    return this.menuModel.find()
+  }
+}
